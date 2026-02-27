00f5beed64b399ed83fa5a221318cfb863343e06
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2026-02-27 09:00:29 +0100

    watch list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,26 @@
+class BookmarksController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render "lists/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
